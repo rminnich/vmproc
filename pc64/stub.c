dea0560883d9a2aec0a89176d2b8b91567e6de4b
g.lenda <rminnich@me.com>
2025-03-27 15:48:22 +0000

    add stub.c since we don't have a screen

---
+++ pc64/stub.c
@@ -0,0 +1,16 @@
+#include	"u.h"
+#include	"../port/lib.h"
+#include	"mem.h"
+#include	"dat.h"
+#include	"fns.h"
+
+/* screen.c */
+char* rgbmask2chan(char *buf, int depth, u32int rm, u32int gm, u32int bm){
+	panic("rgbmask2chan");
+}
+
+/* vgavesa.c */
+char* vesabootscreenconf(char*, char*, uchar*){
+	panic("vesabootscreenconf");
+}
+
